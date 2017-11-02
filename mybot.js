ece5da9a893391375bf71e426f0bffeeaf780a07
rikumax25 <jorgev259@outlook.es>
2017-11-02 09:56:13 -0600

    various fixes

--- mybot.js
+++ mybot.js
@@ -2,23 +2,9 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 var config = require("../data/dvaconfig.json")
-var nicks = require('./nicks.json');
+var nicks = require('../data/nicks.json');
 var fs = require("fs");
-
-
-
-client.on("message", (message) => {  
-    if(message.channel.name == "change-nickname"){
-        var namechange = message.content
-        nicks[message.member.id] = namechange;
-
-        message.member.setNickname(namechange).then(()=>{            
-            fs.writeFile("./nicks.json", JSON.stringify(nicks),"utf-8", function(){});   
-            message.delete(namechange)
-            message.member.removeRole(message.member.removeRole(message.guild.roles.find("name","⭕ Nickname Change")))  
-        })           
-    }
-});
+var util = require("../akirabot/utilities.js")
 
 client.on("guildMemberAdd", (member) => {
     var name = member.user.username;
@@ -35,20 +21,6 @@ client.on("guildMemberAdd", (member) => {
 
 client.on('message', message => {
     var prefix = ">";
-
-     if(message.channel.name == "change-nickname"){
-        var namechange = message.content
-        nicks[message.member.id] = namechange;
-
-        message.member.setNickname(namechange).then(()=>{
-            fs.writeFile("./nicks.json", JSON.stringify(nicks),"utf-8", function(){});
-            message.delete(namechange)
-            message.member.removeRole(message.member.removeRole(message.guild.roles.find("name","⭕ Nickname Change")))
-    }
-
-
-
-
     if(message.content.startsWith(prefix)){
         var command = {};
 
@@ -56,24 +28,30 @@ client.on('message', message => {
         param[0] = param[0].split(prefix)[1];
 
         const commandName = param[0]
+        if(utl.checkPerm(message,commandName)){
+            switch(commandName){
+                case "panic":
+                    message.channel.overwritePermissions(message.guild.roles.get(message.guild.id),{SEND_MESSAGES: false} )
+                    break;
 
-        switch(commandName){
+                case "panicoff":
+                    message.channel.overwritePermissions(message.guild.roles.get(message.guild.id),{SEND_MESSAGES: null} )
+                    break;
+            }
+        }
 
-            case "panic":
-                if(message.member.roles.exists("name","Staff Team")){ //
-                    message.channel.overwritePermissions(message.guild.roles.get(message.guild.id),{SEND_MESSAGES: false} )  
-                }    
-                break;
-                
-                
-                            case "panicoff":
-                if(message.member.roles.exists("name","Staff Team")){ //
-                    message.channel.overwritePermissions(message.guild.roles.get(message.guild.id),{SEND_MESSAGES: null} )  
-                }    
-                break;
-                
-            
-        } 
+        switch(message.channel.name){
+            case "change-nickname":
+                var namechange = message.content + " " + message.member.nickname[message.member.nickname.length -1];
+                nicks[message.member.id] = namechange;
+
+                message.member.setNickname(namechange).then(()=>{
+                    fs.writeFile("./nicks.json", JSON.stringify(nicks),"utf-8", function(){});
+                    message.delete(namechange)
+                    message.member.removeRole(message.member.removeRole(message.guild.roles.find("name","⭕ Nickname Change")))
+                })
+                break
+        }
     }   
 }) 
 
