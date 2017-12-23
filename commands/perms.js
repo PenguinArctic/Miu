faf8051f0ea8b73848765d14ec0249051e487cbc
rikumax25 <jorgev259@outlook.es>
2017-12-23 12:24:55 -0600

    be able to remove permissions

--- commands/perms.js
+++ commands/perms.js
@@ -20,11 +20,28 @@ module.exports = {
                     message.reply(param.join(" ") + " is now allowed to use " + name);
                     break;
 
-                    /*case "remove":
-                            result[0].perms = result[0].perms.filter(e => e !== param.join(" ") );
-                            perms.save(result[0]);
-                            message.reply("Removed " + param.join(" ") + " from the command " + name);
-                            break;*/
+                    case "remove":
+                        if(message.mentions.users.size > 0){
+                            var index = perms[name].user.indexOf(message.mentions.users.first().id);
+                            if(index >= 1){
+                                perms[name].user.splice(index, 1);
+                            }
+                        }else if(message.mentions.channels.size > 0){
+                            var index = perms[name].channel.indexOf(message.mentions.channels.first().id);
+                            if(index >= 1){
+                                perms[name].channel.splice(index, 1);
+                            }
+                        }else{
+                            var index = perms[name].role.indexOf(param.join(" "));
+                            if(index >= 1){
+                                perms[name].role.splice(index, 1);
+                            }
+                        }
+                        
+                        result[0].perms = result[0].perms.filter(e => e !== param.join(" ") );
+                        perms.save(result[0]);
+                        message.reply("Removed " + param.join(" ") + " from the command " + name);
+                        break;
             }
         }else{
             switch(type){
