72ca74df8e558386f859bc268d3ad72c30e67cd4
rikumax25 <jorgev259@outlook.es>
2018-02-21 11:12:32 -0600

    warn Command

---
+++ commands/warn.js
@@ -0,0 +1,22 @@
+var warns = require("../../data/warns.json");
+var util = require('../../akira/utilities.js');
+
+module.exports = {
+    desc:"This is a description",
+    async execute(client, message, param){
+        if(!message.mentions.users.size) return message.channel.send('No user was mentioned')
+        var issued = message.mentions.members.first()
+        if(!warns[issued.id]) warns[issued.id] = []
+
+        var warn = {'reason':'Not defined','issuer':message.author.id}
+        if(param.length>2) warn.reason = param.slice(2,param.length).join(' ')
+        warns[issued.id].push(warn)
+
+        await message.guild.channels.find('name','general').send(`${message.author} issued warn #${warns[issued.id].length} to ${issued}\nReason: ${warn.reason}`)
+        if(warns[issued.id].length >= 3){
+            issued.ban({ reason: warn.reason })
+        } 
+        await util.save(warns,'warns')
+        message.delete()
+    }
+}
